a6b23f500120b6a745e94d60e73485e5751afb7a
myselfanandvp <mailanandvp@gmail.com>
2026-01-14 14:53:49 +0530

    second commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
                 />
                 <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
                 <div className="fixed w-full px-4 py-24 z-50">
-                    <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+                    <div className="max-w-112.5 h-150 mx-auto bg-black/75 text-white">
                         <div className="max-w-[320px] mx-auto py-16">
                             <h1 className="text-3xl font-bold">Sign In</h1>
                             {error && (
